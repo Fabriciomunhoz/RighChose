f5bdab17666149d9d3634d286fe1f82891300ed0
Fabricio Munhoz Badeluk <faluckesdev@protonmail.com>
2024-07-24 11:14:56 -0300

    Adicionar arquivos de projeto.

---
+++ RightChose/Controllers/RightChooseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Newtonsoft.Json;
+
+
+namespace RightChose.Controllers
+{
+    [Route("api/[controller]")]
+    
+    [ApiController]
+    public class RightChooseController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Consulta(double valor)
+        {
+            List<double> valoresComJuros = new List<double>();
+
+            var porcentagemNubank = 0.0526;
+
+            for (int parcelas = 1; parcelas <= 12; parcelas++)
+            {
+                valor = (valor * porcentagemNubank) + valor;
+                valoresComJuros.Add(Math.Round(valor, 2));
+            }
+            return Ok(valoresComJuros);
+        }
+    }
+}
